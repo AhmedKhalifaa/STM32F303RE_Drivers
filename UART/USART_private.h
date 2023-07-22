667ec2472b1a2e7a8a3f1300b1532ca18791c6ab
Ahmed Khalifa <khalifa2048@gmail.com>
2023-07-22 23:54:21 +0300

    Add files via upload

---
+++ UART/USART_private.h
@@ -0,0 +1,20 @@
+
+#ifndef USART_PRIVATE_H
+#define USART_PRIVATE_H
+
+typedef struct{
+	volatile u32 CR[3];
+	volatile u32 BRR;
+	volatile u32 GTPR;
+	volatile u32 RTOR;
+	volatile u32 RQR;
+	volatile u32 ISR;
+	volatile u32 ICR;
+	volatile u32 RDR;
+	volatile u32 TDR;
+}USART_Register;
+
+
+#define 	USART1 		((USART_Register *) 0x40013BFF)
+
+#endif
